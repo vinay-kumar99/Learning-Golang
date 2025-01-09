9fedb70c0d3fb7f1d438861b2bfb0258110c9b6a
Vinay Kumar <vinaykumar.dakshana17@gmail.com>
2025-01-09 15:05:37 +0900

    Added ascii problem

--- Week 1 Fundamentals and Core Syntax/Day1/ascii.go
+++ Week 1 Fundamentals and Core Syntax/Day1/ascii.go
@@ -2,6 +2,7 @@ package main
 
 
 import "fmt"
+import "strconv"
 
 
 func main(){
@@ -9,6 +10,8 @@ func main(){
 	var a int
 
 	fmt.Scan(&a)
-
-
+	
+	s := strconv.Itoa(a);
+	ascii := []rune(s)[0]
+	fmt.Println(ascii);
 }
